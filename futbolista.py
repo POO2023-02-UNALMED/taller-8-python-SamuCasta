0e616892f886fd34854ca63715a61bb02f8eb946
Samuel Castaño Mira <scastanomi@unal.edu.co>
2023-10-16 16:44:00 -0500

    error en el str

--- futbolista.py
+++ futbolista.py
@@ -37,4 +37,4 @@ class Futbolista(Persona, Deportista):
         cls._listaFutbolistas = listaFutbolistas
 
     def __str__(self):
-        return "Mi nombre es",self.getNombre(),"soy profesional en el deporte",self.getDeporte(),"Tengo",self.getEdad(),"años de edad y llevo",self.getAñosPracticando(),"años en el deporte"
+        return f"Mi nombre es {self.getNombre()} soy profesional en el deporte {self.getDeporte()} Tengo {self.getEdad()} años de edad y llevo {self.getAñosPracticando()} años en el deporte"
